561c45b144c396a7ffbc1cedb1543463ff5ac35b
Leart Rustemi <if20b208@technikum-wien.at>
2022-11-12 16:30:37 +0100

    repo

---
+++ src/main/java/at/fhtw/swen3/persistence/repositories/TransferwarehouseRepository.java
@@ -0,0 +1,6 @@
+package at.fhtw.swen3.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferwarehouseRepository extends JpaRepository {
+}
---
+++ src/main/java/at/fhtw/swen3/persistence/repositories/TruckRepository.java
@@ -0,0 +1,6 @@
+package at.fhtw.swen3.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TruckRepository extends JpaRepository {
+}
